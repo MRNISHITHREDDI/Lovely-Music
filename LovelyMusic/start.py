6b3c436ef71a54623e414767c3b43a1133872fcd
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-11-25 11:39:56 +0530

    Update start.py

--- LovelyMusic/start.py
+++ LovelyMusic/start.py
@@ -11,7 +11,7 @@ from config import (
     OWNER_NAME,
     UPDATES_CHANNEL,
 )
-from program import __version__
+from LovelyMusic import __version__
 from driver.filters import command, other_filters
 from pyrogram import Client, filters
 from pyrogram import __version__ as pyrover
