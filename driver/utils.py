934758254264c38f38aea30670f6e41e021f3681
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-11-25 11:06:33 +0530

    Update utils.py

--- driver/utils.py
+++ driver/utils.py
@@ -1,5 +1,5 @@
 from driver.queues import QUEUE, clear_queue, get_queue, pop_an_item
-from driver.veez import call_py
+from driver.lovely import call_py
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.types.input_stream.quality import (
